942c68acb8e8c788d511779892ecd61db01343fd
M-Brachtl <147615009+M-Brachtl@users.noreply.github.com>
2024-03-28 12:41:52 +0100

    new_filtres

--- filtres.py
+++ filtres.py
@@ -1,4 +1,9 @@
 from PIL import Image
+import os
+
+files = os.listdir()
+files.remove('venv')
+files.remove('filtres.py')
 
 def pinkify(path):
     obrazek = Image.open(path)
@@ -14,6 +19,7 @@ def pinkify(path):
             y += 1
         x += 1
     # display(obrazek)
+    obrazek.save("pink_"+path)
     obrazek.show()
 
 def black_white(path):
@@ -30,17 +36,58 @@ def black_white(path):
             y += 1
         x += 1
     # display(obrazek)
+    obrazek.save("bw_"+path)
     obrazek.show()
+
+def greenify(path):
+    image = Image.open(path)
+    width, height = image.size
+
+    for x in range(width):
+        for y in range(height):
+            r, g, b = image.getpixel((x, y))
+            image.putpixel((x, y), (int(r*0.5), int(g*1.5), int(b*0.5)))
+    image.save("green_"+path)
+    image.show()
+
+def blueify(path):
+    image = Image.open(path)
+    width, height = image.size
+
+    for x in range(width):
+        for y in range(height):
+            r, g, b = image.getpixel((x, y))
+            image.putpixel((x, y), (int(r*0.5), int(g*0.5), int(b*1.5)))
+    image.save("blue_"+path)
+    image.show()
+
+def redify(path):
+    image = Image.open(path)
+    width, height = image.size
+
+    for x in range(width):
+        for y in range(height):
+            r, g, b = image.getpixel((x, y))
+            image.putpixel((x, y), (int(r*1.5), int(g*0.5), int(b*0.5)))
+    image.save("green_"+path)
+    image.show()
 
 filters = {
     "pinkify": pinkify,
-    "black-white": black_white
+    "black-white": black_white,
+    "greenify": greenify,
+    "blueify": blueify,
+    "redify": redify
 }
 
 
 print("\nVítej.")
 print("Jaký soubor chceš editovat?")
-image_file = input("./")
+# print("Dostupné soubory:",end=" ")
+# print(*files,sep=", ")
+for i in range(len(files)):
+    print(f"{i+1}: {files[i]}")
+image_file = files[int(input("Zadej číslo obrázku: "))-1]
 print("Dostupné filtry:",end=" ")
 print(*filters.keys(),sep=", ")
 chosen_function = ""
@@ -48,6 +95,3 @@ while not chosen_function in filters.keys():
     chosen_function = input("Vyber filtr: ")
 
 filters[chosen_function](image_file)
-
-
-
