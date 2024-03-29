35e601e978946fa66375993b587c4e8edb612506
Matěj Brachtl <bra42176@spseol.cz>
2024-03-29 14:14:08 +0100

    Blur & Shuffle filtres

--- filtres.py
+++ filtres.py
@@ -1,4 +1,4 @@
-from PIL import Image
+from PIL import Image, ImageFilter
 import os
 
 files = os.listdir()
@@ -69,15 +69,39 @@ def redify(path):
         for y in range(height):
             r, g, b = image.getpixel((x, y))
             image.putpixel((x, y), (int(r*1.5), int(g*0.5), int(b*0.5)))
-    image.save("green_"+path)
+    image.save("red_"+path)
     image.show()
 
+def blur_image(path):
+    obrazek = Image.open(path)
+    blurred = obrazek.filter(ImageFilter.GaussianBlur(5))
+    blurred.save("blurred_" + path)
+    blurred.show()
+
+def color_shuffle(path):
+    obrazek = Image.open(path)
+    sirka, vyska = obrazek.size
+    print('Dostupné styly shuffle: 1-GBR, 2-BRG, 3-RBG, 4-GRB, 5-BGR')
+    choice = int(input("Zadej styl shuffle (1-5): "))-1
+    x = 0
+    while x < sirka:
+        y = 0
+        while y < vyska:
+            r, g, b = obrazek.getpixel((x,y))
+            sh_styles = ((g,b,r),(b,r,g),(r,b,g),(g,r,b),(b,g,r))
+            obrazek.putpixel((x,y), sh_styles[choice])
+            y += 1
+        x += 1
+    obrazek.save("inverted_"+path)
+    obrazek.show()
 filters = {
     "pinkify": pinkify,
     "black-white": black_white,
     "greenify": greenify,
     "blueify": blueify,
-    "redify": redify
+    "redify": redify,
+    "blur": blur_image,
+    "shuffle": color_shuffle
 }
 
 
@@ -95,3 +119,5 @@ while not chosen_function in filters.keys():
     chosen_function = input("Vyber filtr: ")
 
 filters[chosen_function](image_file)
+
+
